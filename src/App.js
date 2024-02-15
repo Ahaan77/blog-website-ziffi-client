9c340b3ec1b5a69db07582f8a9b5a5a55a228dd9
AhaanSehgal <ahaan@threely.io>
2024-02-15 23:07:41 +0530

    Home page Ui complete

--- src/App.js
+++ src/App.js
@@ -3,10 +3,34 @@ import { useEffect } from 'react';
 import { init } from './Backgroud/Init';
 import Topbar from './Components/Topbar';
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
+import { useQuery, gql } from "@apollo/client";
+
 
 function App() {
 
-  const [blogList, setBlogList] = useState(["qsq", "dwd", "dwdwd", "wdhuw", "hdwjdjw", "dhwjd"])
+  const [blogList, setBlogList] = useState([])
+
+  const BLOGS_QUERY = gql`
+  {
+    blogs{
+      title
+      id
+      content
+    }
+  }  
+`;
+
+  const { data, loading, error } = useQuery(BLOGS_QUERY);
+
+  useEffect(() => {
+    if (data) {
+      console.log("check blog resp -->", data?.blogs)
+      setBlogList(data?.blogs)
+    }
+  }, [data])
+
 
   useEffect(() => {
     return init();
@@ -35,17 +59,17 @@ function App() {
           return (
             <div className='flex justify-center'>
               <div style={{ zIndex: 9998 }} className='h-40 w-3/4 mb-10 bg-[#181622] cursor-pointer border border-white border-opacity-30 rounded-xl hover:scale-[1.03] hover:transition duration-200 hover:duration-200'>
-                <p className='text-3xl text-white mt-5 ml-5'>Title of Blog post</p>
-                <p className='text-base mt-3 ml-5 text-white text-opacity-50'> Inki pinki ponky</p>
-
+                <p className='text-3xl text-white mt-5 ml-5'>{blog?.title}</p>
+                <p className='text-base mt-3 ml-5 text-white text-opacity-50'> {blog?.content.slice(0, 300)}...</p>
               </div>
             </div>
           )
         })
-
         }
+      </div>
 
-
+      <div className='h-20 w-20 cursor-pointer bg-[#181622] hover:transition duration-200 hover:duration-200 hover:scale-[1.08] border border-white border-opacity-30 rounded-full fixed bottom-8 right-8 flex items-center justify-center text-white'>
+        <FontAwesomeIcon icon={faPen} size="2x" />
       </div>
     </div>
   );
--- src/Components/Topbar.js
+++ src/Components/Topbar.js
@@ -3,10 +3,10 @@ const Topbar = () => {
     return (
         <div style={{ zIndex: 9999 }} className='h-20 border-b border-white bg-[#181622] border-opacity-20 sticky top-0 flex items-center justify-between'>
             <div className='text-white text-xl mx-10'>
-                Blog Website
+               Chess Blog Website
             </div>
             <div className='text-white text-sm mx-10 font-light flex items-center'>
-                Powered by Ahaan <span className="text-xl ml-1">	&#9749;</span>
+                Powered by Ahaan <span className="text-xl ml-1">&#9749;</span>
             </div>
         </div>
     )
