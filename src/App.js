9f1af3dfc9c6b14899bf06caea9178ea711829c7
AhaanSehgal <ahaan@threely.io>
2024-02-15 21:30:41 +0530

    Componentisation init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect } from 'react';
 import { init } from './Backgroud/Init';
+import Topbar from './Components/Topbar';
 
 function App() {
 
@@ -11,18 +12,14 @@ function App() {
 
   return (
     <div>
-      <div className='h-20 border-b border-white border-opacity-20 sticky top-0 flex items-center justify-between'>
-        <div className='text-white text-xl mx-10'>
-          Blog Website
-        </div>
-      </div>
+      <Topbar />
       <div className='w-full h-screen'>
         <canvas
           style={{
             position: "absolute",
             zIndex: 0,
-            width:"100%",
-            height : "100%"
+            width: "100%",
+            height: "100%"
           }}
           id="canvas"
         ></canvas>
---
+++ src/Components/Topbar.js
@@ -0,0 +1,13 @@
+const Topbar = () => {
+
+    return (
+        <div className='h-20 border-b border-white border-opacity-20 sticky top-0 flex items-center justify-between'>
+            <div className='text-white text-xl mx-10'>
+                Blog Website
+            </div>
+        </div>
+    )
+
+}
+
+export default Topbar
